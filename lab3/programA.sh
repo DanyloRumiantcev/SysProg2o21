a6940bdfd7558002a23199ec077464616727bde6
DanyloRumiantcev <92931323+DanyloRumiantcev@users.noreply.github.com>
2021-11-11 22:02:57 +0200

    Update programA.sh

--- lab3/programA.sh
+++ lab3/programA.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+#Reading string from a console
 read -r -p "Enter a string: " STR
+#Creating a new string, that contains only letters of Latin alphabet
+#That means, no cyrilics, no numbers, no special characters, which is described by [^a-zA-Z]
 STRN=`echo $STR | sed 's/[^a-zA-Z]*//g'`;
+#Now sorting all letters in new string and putting them into a file as a single line
 grep -o . <<< "${STRN}" | sort | tr -d '\n' | tee text.txt
 fi
